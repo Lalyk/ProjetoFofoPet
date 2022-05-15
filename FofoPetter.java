51a22ba55b4098b770c4bae3259bb4a92ea39bc4
unknown <laly.elf@gmail.com>
2022-05-15 13:31:21 -0300

    Projeto I

---
+++ FofoPetter.java
@@ -0,0 +1,72 @@
+package ProjetoFofoPet;
+
+public class FofoPetter {
+	public static void main(String[] args) {
+		
+		CadastroFofoPetter fofopetter1 = new CadastroFofoPetter();
+		CadastroFofoPetter fofopetter2 = new CadastroFofoPetter();
+		CadastroFofoPetter fofopetter3 = new CadastroFofoPetter();
+		CadastroFofoPetter fofopetter4 = new CadastroFofoPetter();
+		CadastroFofoPetter fofopetter5 = new CadastroFofoPetter();
+		
+		
+		System.out.println("-----------------");
+		 fofopetter1.fofoPetter = "Anderson";
+		 fofopetter1.idade = 18;
+		 fofopetter1.endereço = "Rua Herança da Silva, 487 - SP";
+		 fofopetter1.paiMaeDePet = true;
+		 fofopetter1.camasQuentinhas = 2;
+		 fofopetter1.contato = "(11) 4002-8922";
+		 fofopetter1.status();
+		System.out.println("");
+		
+		
+		
+		System.out.println("-----------------");
+		 fofopetter2.fofoPetter = "Leo";
+		 fofopetter2.idade = 30;
+		 fofopetter2.endereço = "Av. Gertulio Tavares Alves, 32 - SP";
+		 fofopetter2.paiMaeDePet = false;
+		 fofopetter2.camasQuentinhas = 1;
+		 fofopetter2.contato = "(21) 92345-9876";
+		 fofopetter2.status();
+		System.out.println("");
+		
+		
+		System.out.println("-----------------");
+		 fofopetter3.fofoPetter = "Augusto";
+		 fofopetter3.idade = 25;
+		 fofopetter3.endereço = "Rua Professor Cesar, 10 - SP";
+		 fofopetter3.paiMaeDePet = true;
+		 fofopetter3.camasQuentinhas = 1;
+		 fofopetter3.contato = "(11) 95678-4321";
+		 fofopetter3.status();
+		System.out.println("");
+		
+		
+		System.out.println("-----------------");
+		 fofopetter4.fofoPetter = "Betina";
+		 fofopetter4.idade = 27;
+		 fofopetter4.endereço = "Rua Mario Widemann, 17 - RS";
+		 fofopetter4.paiMaeDePet = true;
+		 fofopetter4.camasQuentinhas = 10;
+		 fofopetter4.contato = "(53) 91468-9753";
+		 fofopetter4.status();
+		System.out.println("");
+		
+		
+		System.out.println("-----------------");
+		 fofopetter5.fofoPetter = "Tia Gen";
+		 fofopetter5.idade = 40;
+		 fofopetter5.endereço = "Rua Conego Eugenio Leite, 623 - SP";
+		 fofopetter5.paiMaeDePet = true;
+		 fofopetter5.camasQuentinhas = 5;
+		 fofopetter5.contato = "generation.com";
+		 fofopetter5.status();
+		System.out.println("");
+		
+	}
+
+}
+
+
